4d62a48408d411d8afaa17fd5df8c01aeb6aa253
nelson <ultnelscreative1@gmail.com>
2024-03-27 10:14:30 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://Nelson:Gracealloverme123@lofty-blog.j76pcev.mongodb.net/?retryWrites=true&w=majority&appName=lofty-blog"
+  )
+  .then(() => {
+    console.log("MongoDb is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
